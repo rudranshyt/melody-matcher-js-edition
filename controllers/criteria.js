739eccb59fa90d0b385b89f57f4431aec3dbaf7c
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-12-21 16:49:58 +0530

    added dynamic variables

--- controllers/criteria.js
+++ controllers/criteria.js
@@ -8,6 +8,18 @@ function sendEmail() {
   const selectedCategory = document.getElementById("category").value;
   const nameOfUser =
     new URLSearchParams(window.location.search).get("username") || "User";
+  const songsPlaylist = {
+    a: "https://open.spotify.com/playlist/2YsVFAOIyHgaEaIqpoIUFl?si=7241d1fd512143c9",
+    b: "https://open.spotify.com/playlist/5qa7YCiXSmiaZp7vqesd7o?si=8ec033444f4a4809",
+    c: "https://open.spotify.com/playlist/1KZabJbdVkIxXe2kP7O75r?si=0bdc34c23b4e4bc7",
+    d: "https://open.spotify.com/playlist/2W6AmwXjsSo9YxSmKXSs72?si=48d3e455c3c24950",
+    e: "https://open.spotify.com/playlist/4NAMMIsULmfzbFXkvCGRZw?si=4ffdca331da440cf",
+  };
+  let playlistLink;
+
+  if (selectedCategory) {
+    playlistLink = songsPlaylist[selectedCategory];
+  }
 
   if (!emailAddress) {
     alert("Please enter a valid email address.");
@@ -20,9 +32,8 @@ function sendEmail() {
   emailjs
     .send(emailjsConfig.serviceID, emailjsConfig.templateID, {
       to_email: emailAddress,
-      to_name: nameOfUser,
-      category: selectedCategory,
-      message: `Hello ${nameOfUser},\n\nYou selected the category: ${selectedCategory}.\n\nThis is a test email.`,
+      name: nameOfUser,
+      link: playlistLink,
     })
     .then(
       function (response) {
